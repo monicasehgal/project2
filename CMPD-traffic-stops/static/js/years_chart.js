440d7979e4ff1dcd47924dbcd778b1a30534958b
Kegarise <kegarise@queensgranthigh.org>
2018-06-21 15:40:34 -0400

    dashboard aesthetics

--- CMPD-traffic-stops/static/js/years_chart.js
+++ CMPD-traffic-stops/static/js/years_chart.js
@@ -1,18 +1,37 @@
-
-
 function renderYearsChart(data) {
-  var $chartDiv = document.getElementById("years_chart")
+
 
   var years = unpack(data, 0);
   var years_counts = unpack(data, 1);
 
-  var trace3 = {
+  var data = [{
     labels: years,
     values: years_counts,
-    type: "pie"
+    type: "pie",
+    text: years,
+    textinfo: 'label',
+    hoverinfo: 'value+percent',
+    showlegend: false
+  }];
+
+
+
+  var layout = {
+    legend: false,
+   // autosize: false,
+   // width: 350,
+    //height: 350,
+    margin: {
+      l: 0,
+      r: 0,
+      b: 0,
+      t: 0
+    }
   };
 
-  var data = [trace3];
 
-  Plotly.newPlot("years_chart", data);
+  var $PIE = document.getElementById("years_chart");
+  Plotly.newPlot($PIE, data, layout)
+
+
 }
