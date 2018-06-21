8a7e1366526b5f1a4d95e988d6bed0bc514b2308
Kegarise <kegarise@queensgranthigh.org>
2018-06-21 15:50:02 -0400

    organization

--- CMPD-traffic-stops/static/js/years_chart.js
+++ CMPD-traffic-stops/static/js/years_chart.js
@@ -1,37 +1,38 @@
 function renderYearsChart(data) {
 
 
-  var years = unpack(data, 0);
-  var years_counts = unpack(data, 1);
-
-  var data = [{
-    labels: years,
-    values: years_counts,
-    type: "pie",
-    text: years,
-    textinfo: 'label',
-    hoverinfo: 'value+percent',
-    showlegend: false
-  }];
-
-
-
-  var layout = {
-    legend: false,
-   // autosize: false,
-   // width: 350,
-    //height: 350,
-    margin: {
-      l: 0,
-      r: 0,
-      b: 0,
-      t: 0
-    }
-  };
-
-
-  var $PIE = document.getElementById("years_chart");
-  Plotly.newPlot($PIE, data, layout)
-
-
-}
+    var years = unpack(data, 0);
+    var years_counts = unpack(data, 1);
+  
+    var data = [{
+      labels: years,
+      values: years_counts,
+      type: "pie",
+      text: years,
+      textinfo: 'label',
+      hoverinfo: 'value+percent',
+      showlegend: false
+    }];
+  
+  
+  
+    var layout = {
+      legend: false,
+     // autosize: false,
+     // width: 350,
+      //height: 350,
+      margin: {
+        l: 0,
+        r: 0,
+        b: 0,
+        t: 0
+      }
+    };
+  
+  
+    var $PIE = document.getElementById("years_chart");
+    Plotly.newPlot($PIE, data, layout)
+  
+  
+  }
+  
